66fc1a6aa0286d5a0e8234f791b251452c9f8da9
Nick Hoffmann <nickhoffmann0001@gmail.com>
2024-06-25 14:12:36 +0200

    Update Python_Schleifen.py

--- Python_Schleifen.py
+++ Python_Schleifen.py
@@ -1,2 +1,22 @@
 for i in range(10):
     print('Python ist toll!') # 10 mal 'Python ist toll!' ausgeben   
+
+#Aufgabe 2
+for i in range(10):
+    print(i) # 10 mal die Zahlen von 0 bis 9 ausgeben
+
+#Aufgabe 3 + 5
+text = input("Geheime Nachricht: ")
+anzahl = int(input("Wie oft soll der Text ausgegeben werden? "))
+for i in range(anzahl):
+    print(text) # Geheimen Text so oft ausgeben wie der Benutzer möchte
+
+
+text = input("Geheime Nachricht: ")
+anzahl = int(input("Wie oft soll der Text ausgegeben werden? "))
+i=1
+while i <= anzahl:
+    print(text) # Geheimen Text so oft
+    i += 1
+
+#Aufgabe 6
