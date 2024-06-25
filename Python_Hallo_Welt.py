c290396274c1ab53366ce83dd27119318b0afa93
Nick Hoffmann <nickhoffmann0001@gmail.com>
2024-06-25 13:59:47 +0200

    Add files via upload

---
+++ Python_Hallo_Welt.py
@@ -0,0 +1,32 @@
+#Aufgabe 1
+print("Hallo Nick!")
+#Aufgabe 2
+name = input("Wie heißt du? ")
+print("Hallo " + name + "!")
+print("Schön dich zu treffen!")
+#Aufgabe 3
+print(3+3)
+print("3+3")
+print("6+1/2 = ",6+1/2)
+#print(Aloha)
+print("Aloh",'a',"a")
+#Aufgabe 4
+ichBinEinInt = 3
+print("Die Variable 'ichBinEinInt' ist vom Typ",type(ichBinEinInt))
+ichBinEinFloat = 3.5
+print("Die Variable 'ichBinEinFloat' ist vom Typ",type(ichBinEinFloat)) 
+ichBinEinString = "Hallo"
+print("Die Variable 'ichBinEinString' ist vom Typ",type(ichBinEinString))
+ichBinEinBoolean = True
+print("Die Variable 'ichBinEinBoolean' ist vom Typ",type(ichBinEinBoolean))
+#Aufgabe 5
+#Aufgabe 6
+zahl1 = 3
+zahl2 = 5
+print(zahl1 + zahl2)
+print(zahl1 - zahl2)
+#Extra
+zahl1 = input("Gib die erste Zahl ein: ")
+zahl2 = input("Gib die zweite Zahl ein: ")
+print(zahl1 + zahl2)
+#Aufgabe 7
---
+++ Python_Schleifen.py
@@ -0,0 +1,2 @@
+for i in range(10):
+    print('Python ist toll!') # 10 mal 'Python ist toll!' ausgeben   
